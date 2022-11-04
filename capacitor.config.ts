fa651c49664e6d9af2a4eaa20d99d3e6ccc8fffc
daespal <84483704+daespal@users.noreply.github.com>
2022-11-04 07:23:05 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Eje02',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
